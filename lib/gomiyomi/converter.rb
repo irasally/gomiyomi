0a8ceb5a7b88231f0847167cd38a284f4e626648
irasally <irasally@gmail.com>
2019-09-20 13:57:11 +0900

    Add echo.

--- lib/gomiyomi/converter.rb
+++ lib/gomiyomi/converter.rb
@@ -5,9 +5,11 @@ module Gomiyomi
   class Converter
     class << self
       def convert(from_path, to_path)
+        puts "start to convert from #{ from_path } to #{ to_path }."
         csv_reader = CsvReader.new(from_path)
         csv_reader.read_file
         CsvWriter.new(csv_reader.read_rows).write(to_path)
+        puts "..... done!!!"
       end
     end
   end
