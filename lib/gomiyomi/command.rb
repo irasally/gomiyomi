28da908523af8856f140d90c90a88e6efadf5bd3
irasally <irasally@gmail.com>
2019-09-20 13:52:27 +0900

    Fix variable names.

--- lib/gomiyomi/command.rb
+++ lib/gomiyomi/command.rb
@@ -13,7 +13,7 @@ module Gomiyomi
     end
 
     desc "convert SAPPORO-GARVAGE-DATA-CSV-PATH OUTPUT-DATA-CSV-PATH", "Convert to sapporo.5374.jp csv data."
-    def convert(from, to)
+    def convert(from_path, to_path)
       puts Converter.convert(from_path, to_path)
     end
   end
